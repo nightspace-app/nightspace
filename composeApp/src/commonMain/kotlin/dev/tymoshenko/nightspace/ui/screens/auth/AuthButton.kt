2096bf1c42a9e2b5330061a8851143c26aff4c09
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-09-25 21:20:43 +0200

    Made Initial screen UI

---
+++ composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/AuthButton.kt
@@ -0,0 +1,53 @@
+package dev.tymoshenko.nightspace.ui.screens.auth
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AuthButton(
+    onClick: () -> Unit,
+    text: String,
+    leadingIcon: (@Composable () -> Unit)? = null
+) {
+    Button(
+        modifier = Modifier
+            .fillMaxWidth(0.9f),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors().copy(
+            containerColor = Color.DarkGray
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+            ) {
+                leadingIcon?.invoke()
+            }
+
+            Text(
+                text = text,
+                fontStyle = FontStyle.Italic,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/initial/Initial.kt
+++ composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/initial/Initial.kt
@@ -1,18 +1,27 @@
 package dev.tymoshenko.nightspace.ui.screens.auth.initial
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import dev.tymoshenko.nightspace.nav.screens.AuthScreens
+import dev.tymoshenko.nightspace.ui.screens.auth.AuthButton
 import nightspace.composeapp.generated.resources.Res
 import nightspace.composeapp.generated.resources.auth_img
+import nightspace.composeapp.generated.resources.google_icon
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
@@ -21,18 +30,48 @@ fun Initial(navController: NavHostController) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Column {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Image(
                 painter = painterResource(Res.drawable.auth_img),
                 contentDescription = ""
             )
 
-            Text(
-                modifier = Modifier
-                .clickable {
-                    navController.navigate(AuthScreens.Register)
-                }, text = "fas"
+            Spacer(Modifier.size(24.dp))
+
+            AuthButton(
+                onClick = { },
+                text = "Sign in with Google",
+                leadingIcon = {
+                    Image(
+                        modifier = Modifier
+                            .size(24.dp),
+                        painter = painterResource(Res.drawable.google_icon),
+                        contentDescription = ""
+                    )
+                }
             )
+
+            Spacer(Modifier.size(8.dp))
+
+            AuthButton(
+                onClick = { navController.navigate(AuthScreens.Login) },
+                text = "Sign in with Email"
+            )
+
+            Spacer(Modifier.size(8.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                TextButton(onClick = { navController.navigate(AuthScreens.Register) }) {
+                    Text("Register with Email")
+                }
+            }
         }
     }
 }
