8364498fd5c445925e946c34e15548838e39ae83
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2025-09-25 20:39:21 +0200

    Changed nav host anim

--- composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/AuthHost.kt
+++ composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/AuthHost.kt
@@ -1,11 +1,14 @@
 package dev.tymoshenko.nightspace.ui.screens.auth
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import dev.tymoshenko.nightspace.nav.authGraph
@@ -16,6 +19,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun AuthHost() {
     val navController = rememberNavController()
     val source = Color(0xFF21243F)
+    val animationSpec = tween<IntOffset>(500)
 
     Box(
         modifier = Modifier
@@ -23,7 +27,14 @@ fun AuthHost() {
             .background(source)
     )
 
-    NavHost(navController = navController, startDestination = AuthScreens.Initial) {
+    NavHost(
+        navController = navController,
+        startDestination = AuthScreens.Initial,
+        enterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Left, animationSpec) },
+        exitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Left, animationSpec) },
+        popEnterTransition = { slideIntoContainer(AnimatedContentTransitionScope.SlideDirection.Right, animationSpec) },
+        popExitTransition = { slideOutOfContainer(AnimatedContentTransitionScope.SlideDirection.Right, animationSpec) }
+    ) {
         authGraph(navController)
     }
 }
--- composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/initial/Initial.kt
+++ composeApp/src/commonMain/kotlin/dev/tymoshenko/nightspace/ui/screens/auth/initial/Initial.kt
@@ -1,20 +1,19 @@
 package dev.tymoshenko.nightspace.ui.screens.auth.initial
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import dev.tymoshenko.nightspace.nav.screens.AuthScreens
-import dev.tymoshenko.nightspace.nav.screens.Screens
-import org.jetbrains.compose.resources.painterResource
-
 import nightspace.composeapp.generated.resources.Res
 import nightspace.composeapp.generated.resources.auth_img
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun Initial(navController: NavHostController) {
@@ -22,13 +21,18 @@ fun Initial(navController: NavHostController) {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Image(
-            modifier = Modifier
+        Column {
+            Image(
+                painter = painterResource(Res.drawable.auth_img),
+                contentDescription = ""
+            )
+
+            Text(
+                modifier = Modifier
                 .clickable {
                     navController.navigate(AuthScreens.Register)
-                },
-            painter = painterResource(Res.drawable.auth_img),
-            contentDescription = ""
-        )
+                }, text = "fas"
+            )
+        }
     }
 }
